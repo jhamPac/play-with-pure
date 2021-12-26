1464f7af83495faa739a6b62df8c4016c64ad1f2
jhamPac <cloud@kevia.me>
2021-12-26 16:46:13 -0700

    chore: save point

--- spago.dhall
+++ spago.dhall
@@ -12,13 +12,15 @@ to generate this file without the comments in this block.
 -}
 { name = "my-project"
 , dependencies =
-  [ "console"
+  [ "arrays"
+  , "console"
   , "control"
   , "effect"
   , "lists"
   , "maybe"
   , "prelude"
   , "psci-support"
+  , "tuples"
   ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -2,13 +2,12 @@ module Data.AddressBook where
 
 import Prelude
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, (:))
 import Data.Maybe (Maybe)
 
 type Entry
   = { firstName :: String
     , lastName :: String
-    , address :: Address
     }
 
 type Address
@@ -16,3 +15,6 @@ type Address
     , city :: String
     , state :: String
     }
+
+fullName :: Entry -> String
+fullName { firstName: f, lastName: l } = "Hello, " <> f <> " " <> l
