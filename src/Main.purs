b8cf6ddcc546e610934dd68953fb5d42635c8cf6
jhamPac <cloud@kevia.me>
2021-12-26 17:21:55 -0700

    chore: serve html

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,16 @@
 module Main where
 
 import Prelude
-
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.VDom.Driver (runUI)
+import UI.Counter (component)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main =
+  HA.runHalogenAff do
+    body <- HA.awaitBody
+    runUI component unit body
---
+++ src/UI/Counter.purs
@@ -0,0 +1,31 @@
+module UI.Counter where
+
+import Prelude
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+
+data Action
+  = Increment
+  | Decrement
+
+component :: forall query input output m. H.Component query input output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval H.defaultEval { handleAction = handleAction }
+    }
+  where
+  initialState _ = 0
+
+  render state =
+    HH.div_
+      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
+      , HH.text (show state)
+      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "*" ]
+      ]
+
+  handleAction = case _ of
+    Decrement -> H.modify_ \state -> state - 1
+    Increment -> H.modify_ \state -> state + 1
